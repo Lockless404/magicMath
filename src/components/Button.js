c2e2ffd597361254c97e8bb7070f2d1c33e3938c
Hamza <hamzatarar8@gmail.com>
2021-12-03 23:16:43 +0200

    added functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ text, color, className }) => (
+const Button = ({ text, color, className, onClick }) => (
   <>
-    <button type="button" className={className} style={{ backgroundColor: color }}>{text}</button>
+    <button type="button" className={className} style={{ backgroundColor: color }} onClick={onClick}>{text} </button>
   </>
 );
 
@@ -10,6 +10,7 @@ Button.propTypes = {
   text: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
