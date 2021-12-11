f74b23096091b728b32409bc7e468cc7563b8cd2
Hamza <hamzatarar8@gmail.com>
2021-12-11 18:09:59 +0200

    Completed Full webpage

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -27,28 +27,31 @@ const Calculator = () => {
     return 0;
   };
   return (
-    <>
-      <input className="calcDisplay" placeholder="0" type="text" maxLength="0" value={display()} onChange={() => ''} />
-      <Button onClick={clicked} className="button AC" text="AC" color="lightgrey" />
-      <Button onClick={clicked} className="button +-" text="+/-" color="lightgrey" />
-      <Button onClick={clicked} className="button %" text="%" color="lightgrey" />
-      <Button onClick={clicked} className="button /" text="÷" color="coral" />
-      <Button onClick={clicked} className="button 7" text="7" color="lightgrey" />
-      <Button onClick={clicked} className="button 8" text="8" color="lightgrey" />
-      <Button onClick={clicked} className="button 9" text="9" color="lightgrey" />
-      <Button onClick={clicked} className="button x" text="x" color="coral" />
-      <Button onClick={clicked} className="button 4" text="4" color="lightgrey" />
-      <Button onClick={clicked} className="button 5" text="5" color="lightgrey" />
-      <Button onClick={clicked} className="button 6" text="6" color="lightgrey" />
-      <Button onClick={clicked} className="button -" text="-" color="coral" />
-      <Button onClick={clicked} className="button 1" text="1" color="lightgrey" />
-      <Button onClick={clicked} className="button 2" text="2" color="lightgrey" />
-      <Button onClick={clicked} className="button 3" text="3" color="lightgrey" />
-      <Button onClick={clicked} className="button +" text="+" color="coral" />
-      <Button onClick={clicked} className="button O" text="0" color="lightgrey" />
-      <Button onClick={clicked} className="button ." text="." color="lightgrey" />
-      <Button onClick={clicked} className="button =" text="=" color="coral" />
-    </>
+    <div>
+      <h1>Lets Do Some Math!</h1>
+      <div className='calc'>
+        <input className="calcDisplay" placeholder="0" type="text" maxLength="0" value={display()} onChange={() => ''} />
+        <Button onClick={clicked} className="button AC" text="AC" color="lightgrey" />
+        <Button onClick={clicked} className="button +-" text="+/-" color="lightgrey" />
+        <Button onClick={clicked} className="button %" text="%" color="lightgrey" />
+        <Button onClick={clicked} className="button /" text="÷" color="coral" />
+        <Button onClick={clicked} className="button 7" text="7" color="lightgrey" />
+        <Button onClick={clicked} className="button 8" text="8" color="lightgrey" />
+        <Button onClick={clicked} className="button 9" text="9" color="lightgrey" />
+        <Button onClick={clicked} className="button x" text="x" color="coral" />
+        <Button onClick={clicked} className="button 4" text="4" color="lightgrey" />
+        <Button onClick={clicked} className="button 5" text="5" color="lightgrey" />
+        <Button onClick={clicked} className="button 6" text="6" color="lightgrey" />
+        <Button onClick={clicked} className="button -" text="-" color="coral" />
+        <Button onClick={clicked} className="button 1" text="1" color="lightgrey" />
+        <Button onClick={clicked} className="button 2" text="2" color="lightgrey" />
+        <Button onClick={clicked} className="button 3" text="3" color="lightgrey" />
+        <Button onClick={clicked} className="button +" text="+" color="coral" />
+        <Button onClick={clicked} className="button O" text="0" color="lightgrey" />
+        <Button onClick={clicked} className="button ." text="." color="lightgrey" />
+        <Button onClick={clicked} className="button =" text="=" color="coral" />
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-wrapper">
+      <h1 className="home-title"> Welcome to our Page!</h1>
+      <p className="para">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Eligendi non quis exercitationem culpa nesciunt nihil aut 
+        nostrum explicabo reprehenderit optio amet ab temporibus a
+        speriores quasi cupiditate. Voluptatum ducimus voluptates
+         voluptas?
+      </p>
+      <p className="para">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Eligendi non quis exercitationem culpa nesciunt nihil aut 
+        nostrum explicabo reprehenderit optio amet ab temporibus a
+        speriores quasi cupiditate. Voluptatum ducimus voluptates
+         voluptas?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
