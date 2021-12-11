063c02fce2903cdf36e962e7159fdbe269d370c8
Hamza <hamzatarar8@gmail.com>
2021-12-11 18:25:16 +0200

    Corrected linters

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home() {
   return (
     <div>
       <h1> Welcome to our Page!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
     </div>
   );
 }
